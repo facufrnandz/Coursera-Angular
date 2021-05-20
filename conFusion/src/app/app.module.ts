1cf17473d46b0ed088847e1b8cb111f6071e8913
facufrnandz <facufrnandz@gmail.com>
2021-05-20 11:52:51 -0300

    Components Part 2

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -7,6 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
 import { MatListModule } from '@angular/material/list';
@@ -18,6 +23,9 @@ import { MatListModule } from '@angular/material/list';
   ],
   imports: [
     
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
