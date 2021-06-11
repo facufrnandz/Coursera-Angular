5da51502c2fede6f69c65b58bb972bb5b337f69c
facufrnandz <facufrnandz@gmail.com>
2021-06-11 17:58:59 -0300

    Dialogs

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -1,31 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar'; 
 import { FlexLayoutModule } from '@angular/flex-layout';
-
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatListModule } from '@angular/material/list';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { MenuComponent } from './menu/menu.component';
-import { MatListModule } from '@angular/material/list';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
-import { DishService } from './services/dish.service';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
 
 import 'hammerjs';
 import { PromotionService } from './services/promotion.service';
 import { LeaderService } from './services/leader.service';
+import { DishService } from './services/dish.service';
 
 
 @NgModule({
@@ -37,7 +36,8 @@ import { LeaderService } from './services/leader.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     MatGridListModule,
@@ -48,7 +48,11 @@ import { LeaderService } from './services/leader.service';
     MatToolbarModule,
     MatListModule,
     FlexLayoutModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDialogModule
+  ],
+  entryComponents: [
+    LoginComponent
   ],
   providers: [
     DishService,
